52503bac13d68f639029c2c9619b62126d726ddb
FilipPietryga <filippatrykpietryga@gmail.com>
2022-11-11 21:36:38 +0100

    solution

--- app/services/users-service.js
+++ app/services/users-service.js
@@ -1,6 +1,7 @@
 'use strict';
 const ServiceResult = require('../models/service-results/service-results');
 const UsersErrors = require('../constants/errors/user-errors');
+const Pagination = require('../utilities/pagination/pagination');
 
 class UsersService {
   /**
@@ -11,8 +12,19 @@ class UsersService {
   async createUser(createUserDTO) {
     // todo: Implement create user service method. It should adds user to database.
     // todo: Look at createUserDTO properties for user input
+    try {
+      let user = await DB.User.create({
+        email: createUserDTO.email,
+        firstName: createUserDTO.firstName,
+        lastName: createUserDTO.lastName,
+        password: createUserDTO.password
+      });
 
-    return new ServiceResult(UsersErrors.NOT_IMPLEMENTED);
+      return new ServiceResult(null, user);
+    } catch (error) {
+      console.error(error);
+      return new ServiceResult(UsersErrors.UNEXPECTED_ERROR);
+    }
   }
 
   /**
@@ -23,8 +35,17 @@ class UsersService {
   async getUsersList(getUsersListDTO) {
     // todo: Implement get users list service method. It should gets users list from database.
     // todo: Look at getUsersListDTO properties for user input
+    try {
+      let pagination = new Pagination(getUsersListDTO, 1, 10, 100);
+      let users = await (async function (offset, limit) {
+        return DB.User.findAll({offset, limit});
+      })(pagination.offset, pagination.pageSize);
 
-    return new ServiceResult(UsersErrors.NOT_IMPLEMENTED);
+      return new ServiceResult(null, users);
+    } catch (error) {
+      console.error(error);
+      return new ServiceResult(UsersErrors.UNEXPECTED_ERROR);
+    }
   }
 
   /**
@@ -32,11 +53,23 @@ class UsersService {
    * @param {GetSingleUserDTO} getSingleUserDTO - get single user dto
    * @return {Promise<ServiceResult<DB.User>>} - service result
    */
-  async getSingleUser(getSingleUserDTO) {
+  async getSingleUser(GetSingleUserDTO) {
     // todo: Implement get single user service method. It should gets single user (by id) from database.
     // todo: Look at getSingleUserDTO properties for user input
+    try {
+      let user = await (async function (id) {
+        return DB.User.findOne({id});
+      })(GetSingleUserDTO.userId);
+      
+      if (!user) {
+        return new ServiceResult(UsersErrors.NOT_FOUND);
+      }
 
-    return new ServiceResult(UsersErrors.NOT_IMPLEMENTED);
+      return new ServiceResult(null, user);
+    } catch (error) {
+      console.error(error);
+      return new ServiceResult(UsersErrors.UNEXPECTED_ERROR);
+    }
   }
 }
 
